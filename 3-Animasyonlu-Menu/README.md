e58f6fe04cd402da5fc983b643544364a9ed375a
Burak Zafer Özcan <124523302+burakzaferozcan@users.noreply.github.com>
2023-06-15 13:16:54 +0300

    Add files via upload

---
+++ 3-Animasyonlu-Menu/README.md
@@ -0,0 +1,4 @@
+# Animasyonlu-Menu
+ Javascript ile oluşturduğum basit bir animasyonlu menu.
+
+Proje Linki : https://648ae42cf7552a00bf1472cb--effulgent-melba-b189d3.netlify.app/
---
+++ 3-Animasyonlu-Menu/script.js
@@ -0,0 +1,5 @@
+const circle=document.querySelector(".circle");
+const container=document.querySelector(".container")
+circle.addEventListener("click",()=>{
+    container.classList.toggle("show-nav")
+})
